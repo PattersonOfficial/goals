d6db3ce4397a3ef94df02493301548020b78b542
Benjamin Akrong <akrongwork@gmail.com>
2023-01-20 17:34:37 +0000

    added redirect pathd to backend to display client side on server

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Goals App
+# Goals Progression App
 Creating a Goals application using the mern stack with authentication. This is to ensure and enable an in-depth understanding and relearning of mern stack
+
+# Backend Development
+npm install
+
+# Frontend Development
+cd frontend
+npm install
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middlewares/errorMiddleware');
@@ -13,6 +14,19 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goal-routes'));
 app.use('/api/users', require('./routes/user-routes'));
+
+// serving from frontend folder
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set environment to production'));
+}
 
 app.use(errorHandler);
 
